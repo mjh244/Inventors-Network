145ad264bae506ff468af23e2e91c48310617c3c
Alexander Mazon <axm1106@case.edu>
2021-04-03 19:45:13 -0400

    added back the metric caclculations that we will need to feed into our dataset. (I think we should be ready for the ml now).

--- inventors_network.py
+++ inventors_network.py
@@ -126,14 +126,68 @@ for i in range(len(df)):
     if (patents[i], inventors[i]) not in inventorNetwork.edges:
         inventorNetwork.add_edge(patents[i], inventors[i])
 
+#####################################
+# Metrics Calculation
+#####################################
 
+# Computes metrics for nodes
+degree = nx.degree_centrality(inventorNetwork)
+print("degree calculated")
+closeness = nx.closeness_centrality(inventorNetwork)
+print("closeness calculated")
+betweenness = nx.betweenness_centrality(inventorNetwork, 25, normalized = True, endpoints = False)
+print("betweenness calculated")
+eigenvector = nx.eigenvector_centrality(inventorNetwork, max_iter=1000)
+print("eigenvector calculated")
+
+# Lists to hold company centrality metrics
+degreePerCompany = []
+closenessPerCompany = []
+betweennessPerCompany = []
+eigenvectorPerCompany = []
+companies = companies.tolist()
+
+#just trying to figure out the bounds
+print("companies")
+print(len(companies))
+print(len(degree))
+print(len(closeness))
+print(len(betweenness))
+print(len(eigenvector))
+
+# Gets the metrics based on company key and stores them in order of company to merge with df
+#this was changed to loop through the length of companies so we dont get an out of bounds error
+for i in range(len(companies)):
+    degreePerCompany.append(degree.get(companies[i]))
+    closenessPerCompany.append(closeness.get(companies[i]))
+    betweennessPerCompany.append(betweenness.get(companies[i]))
+    eigenvectorPerCompany.append(eigenvector.get(companies[i]))
+
+print("Finished computing lists \n")
+
+# Limits df to number of companies centrality was calculated for
+df = df.head(len(degree))
+
+# Adds the metrics to the dataframe
+df['Degree'] = degreePerCompany
+df['Closeness'] = closenessPerCompany
+df['Betweeness'] = betweennessPerCompany
+df['Eigenvector'] = eigenvectorPerCompany
+
+# Drops rows with empty fields and saves the df
+df = df.dropna()
+#dataframe with metrics shown
+print("Dataframe with metrics")
+print(df)
+df.to_csv('inventor-patent-ten-thousand-nodes.csv')
 # Gathers company names to gather nodes
 allNodesByCompany = inventorNetwork.nodes(data = True)
 allNodesByCompany2 = list(inventorNetwork.nodes)
-nodeNames = []
+
 
 #Sets node limit for subset network, I had to set it to 10 so it would be small
 # enough given the initial size of our df was cut down by so much
+nodeNames = []
 nodeSubsetLimit = 10
 for i in range(nodeSubsetLimit):
     nodeNames.append(allNodesByCompany2[i])
