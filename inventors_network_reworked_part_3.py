2fd0b3eb0be283516a54991f2855fb645f522ec7
Alexander Mazon <axm1106@case.edu>
2021-05-05 21:54:28 -0400

    added the skeleton structre for determining optimal model parameters using gridsearch.

--- inventors_network_reworked_part_3.py
+++ inventors_network_reworked_part_3.py
@@ -15,7 +15,7 @@ from sklearn import svm
 from sklearn import tree
 from sklearn.neural_network import MLPClassifier
 from sklearn.model_selection import validation_curve
-
+from sklearn.model_selection import GridSearchCV
 from pandas import read_csv
 from seaborn import scatterplot
 from matplotlib import pyplot as plt
@@ -60,11 +60,24 @@ features = np.asarray(features)
 features = features.astype(np.float64)
 labels = np.asarray(labels)
 labels = labels.astype(np.float64)
+features_train, features_test, labels_train, labels_test = train_test_split(features, labels, test_size=0.2)
 #analysis on tree depth
 print("params")
 classifierTree = tree.DecisionTreeClassifier()
+params = classifierTree.get_params()
+print(params)
+#skeleton for parameter tuning
+parameters = {'criterion': ["gini","random"],
+              'splitter': ["best", "random"],
+              'max_depth': np.arange(1,20)}
 
-print(classifierTree.get_params())
+grid_tree = GridSearchCV(estimator=classifierTree, param_grid=parameters, cv=5, scoring="accuracy")
+grid_tree.fit(features_train, labels_train)
+print("results from grid search")
+print("The best estimator across ALL searched params:\n",grid_tree.best_estimator_)
+print("The best score across ALL searched params:\n",grid_tree.best_score_)
+print("The best parameters across ALL searched params:\n",grid_tree.best_params_)
+
 para_range = np.arange(1,20)
 train_scores, validation_scores = validation_curve(classifierTree, X=features, y=labels, param_name="max_depth",
                                                    param_range=para_range, scoring="accuracy")
