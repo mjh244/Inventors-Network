23f6dd50789c2236c22c95f3b5e69fe6fa710591
Haolin Hu <hxh435@case.edu>
2021-05-10 22:39:05 +0800

    Update inventors_network_reworked_part_4.py

--- inventors_network_reworked_part_4.py
+++ inventors_network_reworked_part_4.py
@@ -4,16 +4,18 @@ from numpy import array
 import numpy as np
 from sklearn import preprocessing
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.naive_bayes import GaussianNB
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import precision_score
 from sklearn.metrics import recall_score
-from sklearn import svm
+from sklearn.svm import SVC
 from sklearn import tree
 from sklearn.neural_network import MLPClassifier
 from sklearn.model_selection import validation_curve
 from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import RandomizedSearchCV
 from pandas import read_csv
 from seaborn import scatterplot
 from matplotlib import pyplot as plt
@@ -26,13 +28,13 @@ with open('datasets/inventor-patent-tickers-dates-prices-centrality-to-numbers.c
   for line in csv.reader(file):
       #print(len(line))
       if (line[0] != "Firstname"):
-        features.append(line[0:16]+line[17:20])
+        features.append(line[0:16] +line[17:20])
         labels.append(line[16])
 
 # Prints out length of entries and some lables
-print(len(features))
-print(features[0])
-print(labels[0:25])
+#print(len(features))
+#print(features[0])
+#print(labels[0:25])
 
 # Turns features and labels to floats to perform ML
 features = np.asarray(features)
@@ -41,17 +43,30 @@ labels = np.asarray(labels)
 labels = labels.astype(np.float64)
 features_train, features_test, labels_train, labels_test = train_test_split(features, labels, test_size=0.2)
 
-model = RandomForestClassifier(n_estimators = 100, bootstrap= True, max_depth = 8, max_features= 'sqrt')
-params = model.get_params()
+param_dist = {'min_samples_split': np.arange(6,9)}
+model = RandomForestClassifier(n_estimators = 100, bootstrap= False, min_samples_leaf = 7, max_depth = 7, max_features= 'sqrt')
+#model = GradientBoostingClassifier(n_estimators=20, learning_rate=1, max_features=2, max_depth=2, random_state=0)
+#model = SVC(kernel="linear")
+#model = GaussianNB()
 
-para_range = np.arange(5,12)
+"""
+cv = GridSearchCV(model, param_grid= param_dist, cv=5, scoring="accuracy")
+cv.fit(features_train, labels_train)
+print("results from grid search")
+print("The best estimator across ALL searched params:\n",cv.best_estimator_)
+print("The best score across ALL searched params:\n",cv.best_score_)
+print("The best parameters across ALL searched params:\n",cv.best_params_)
+"""
+
+para_range = np.arange(4, 11)
 
 model.fit(features_train, labels_train)
 prediction = model.predict(features_test)
 print(accuracy_score(labels_test, prediction))
 
+
 """
-train_scores, validation_scores = validation_curve(model, X=features, y=labels, param_name="max_depth",
+train_scores, validation_scores = validation_curve(model, X=features, y=labels, param_name="min_samples_leaf",
                                                    param_range=para_range, scoring="accuracy")
 mean_train_score = np.mean(train_scores, axis = 1)
 std_train_score = np.std(train_scores, axis = 1)
@@ -62,11 +77,11 @@ plt.plot(para_range, mean_train_score, label="Training Score", color='b')
 plt.plot(para_range, mean_test_score, label="Cross Validation Score", color='g')
 
 plt.ylabel("Accuracy")
-plt.xlabel("Depth of Tree")
+plt.xlabel("min_samples_split")
 plt.title("Validation scores for tree")
 plt.tight_layout()
 plt.legend(loc = 'best')
-plt.savefig('figures/Validation_scores_for_tree.png')
+#plt.savefig('figures/Validation_scores_for_tree.png')
 plt.show()
 print("finished validation analysis")
 """
