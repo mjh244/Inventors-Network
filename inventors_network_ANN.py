2a8981e5e0b29c9af499bd83ac3f8188d252a898
Alexander Mazon <axm1106@case.edu>
2021-05-15 19:59:45 -0400

    Added code for optimization of the ANN via hyperparameter tuning, and output figures corresponding to the tuned model.

--- inventors_network_ANN.py
+++ inventors_network_ANN.py
@@ -1,72 +1,32 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import validation_curve
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import RandomizedSearchCV
 from sklearn.model_selection import GridSearchCV
 from keras.wrappers.scikit_learn import KerasClassifier
+from sklearn.metrics import confusion_matrix
+import seaborn as sn
 
 
 # Importing the dataset
-dataset = pd.read_csv('datasets/inventor-patent-tickers-dates-prices-centrality.csv')
-#X = dataset.iloc[:, [1:10], [11:18], [20:23]].values
 
-
-
-for col in dataset.keys():
-  if ((col == 'Firstname') or (col == 'Lastname') or (col == 'City') or (col == 'State') or (col == 'Country')
-  or (col == 'Zipcode') or (col == 'Patent') or (col == 'AppYear') or (col == 'GYear')  or (col == 'Assignee')
-  or (col == 'Tickers') or (col == 'Month Before Application Date') or (col == 'Application Date')
-  or (col == 'Month After Application Date')):
-    dataset[col] = pd.Categorical(dataset[col], ordered=True).codes
-    pd.to_numeric(dataset[col], downcast='float')
-'''
-new_df = pd.DataFrame(dataset["Price a Month Before"], columns = ["Price a Month Before"])
-new_df["Price the Day of"] = dataset["Price the Day of"]
-new_df["Change"] = dataset["Change"]
-new_df["Degree"] = dataset["Degree"]
-new_df["Betweenness"] = dataset["Betweenness"]
-new_df["Eigenvector"] = dataset["Eigenvector"]
-
-print(new_df)
-'''
-dropped_change = dataset.drop(columns="Change")
-dropped_AppDate = dropped_change.drop(columns="AppDate")
-dropped_price_month_after = dropped_AppDate.drop(columns="Price a Month After")
-# Turns the dataframe to numbers
-#df = df.apply(pd.to_numeric, downcast='float')
-#df = pd.to_numeric(df, downcast='float')
-new_df = dropped_price_month_after.to_numpy()
-sc = StandardScaler()
-# Normalizes the dataframe
-#min_max_df = MinMaxScaler().fit_transform(new_df)
-standard_scaler_df = sc.fit_transform(new_df)
-new_df= pd.DataFrame(standard_scaler_df)
-new_df["Change"]=dataset["Change"]
-#new_df = pd.DataFrame(min_max_df)
-
-#print(new_df)
-
-#print(max(list(new_df[0])))
-#print(max(list(new_df[1])))
-#print(max(list(new_df[2])))
-#print(max(list(new_df[3])))
-# Saves the dataframe with values converted to numbers to be used in ML
-#new_df.to_csv('datasets/inventor-patent-tickers-dates-prices-centrality-to-numbers.csv', index=False)
+new_df = pd.read_csv('datasets/inventor-patent-tickers-dates-prices-centrality-to-numbers.csv')
 print("Dataset with values converted to numbers")
 print(new_df)
 
 #X = new_df.iloc[:, ["Firstname","Lastname","City","State","Country","Zipcode","Patent","AppYear","GYear","Assignee",
                     #"Tickers","Month Before Application Date","Application Date","Month After Application Date",
                     #"Price a Month Before","Price the Day of","Degree","Betweenness","Eigenvector"]].values
-X = new_df.iloc[:, [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19]].values
+X = new_df.iloc[:, 0:19].values
 #X = dataset.iloc[:, [13,14,15,16,17,20,21,22]].values
 #X = new_df.iloc[:, [0,1,3,4,5]].values
 #y = new_df.iloc[:, 2].values
 #y = new_df.iloc[:, "Change"].values
-y= new_df.iloc[:, 20].values
+y= new_df.iloc[:, 19].values
 # Encoding categorical data
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 from sklearn.compose import ColumnTransformer
@@ -149,6 +109,7 @@ def create_classifier(init_mode='uniform', num_nodes=3):
     # Compiling the ANN
     classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['acc'])
     return classifier
+
 def create_classifier2(optimizer='adam', init='glorot_uniform'):
     classifier = keras.Sequential()
     print("classifier initialized")
@@ -168,15 +129,17 @@ def create_classifier2(optimizer='adam', init='glorot_uniform'):
     # Compiling the ANN
     classifier.compile(optimizer=optimizer, loss='binary_crossentropy', metrics=['acc'])
     return classifier
-print("fitting")
 seed = 7
 np.random.seed(seed)
+# analysis on ANN
 #batch_size = 128
 #epochs = 10
 # hyperparameter tuning comes from https://towardsdatascience.com/simple-guide-to-hyperparameter-tuning-in-neural-networks-3fe03dad8594
 #model_CV = KerasClassifier(build_fn=create_classifier, epochs=epochs,
                            #batch_size=batch_size, verbose=1)
+'''
 model_init_epochs_batch_CV = KerasClassifier(build_fn=create_classifier2, verbose=1)
+
 init_mode = ['glorot_uniform', 'glorot_normal']
 batches = [68, 128, 256, 512]
 epochs = [10, 20, 30, 40, 50, 60]
@@ -185,7 +148,7 @@ epochs = [10, 20, 30, 40, 50, 60]
              #'glorot_normal', 'glorot_uniform', 'he_normal', 'he_uniform']
 
 param_grid = dict(epochs=epochs, batch_size=batches, init=init_mode)
-#grid = GridSearchCV(estimator=model_CV, param_grid=param_grid, n_jobs=-1, cv=3)
+
 #grid = GridSearchCV(estimator=model_init_epochs_batch_CV,
                     #param_grid=param_grid,
                     #cv=3)
@@ -194,8 +157,9 @@ grid = RandomizedSearchCV(estimator=model_init_epochs_batch_CV,
                     param_distributions=param_grid,
                     cv=3)
 grid_result = grid.fit(X_train, y_train)
+
 # print results
-'''
+
 print(f'Best Accuracy for {grid_result.best_score_} using {grid_result.best_params_}')
 means = grid_result.cv_results_['mean_test_score']
 stds = grid_result.cv_results_['std_test_score']
@@ -203,7 +167,7 @@ params = grid_result.cv_results_['params']
 for mean, stdev, param in zip(means, stds, params):
     print(f' mean={mean:.4}, std={stdev:.4} using {param}')
 
-'''
+
 # print results
 print(f'Best Accuracy for {grid_result.best_score_:.4} using {grid_result.best_params_}')
 means = grid_result.cv_results_['mean_test_score']
@@ -211,7 +175,7 @@ stds = grid_result.cv_results_['std_test_score']
 params = grid_result.cv_results_['params']
 for mean, stdev, param in zip(means, stds, params):
     print(f'mean={mean:.4}, std={stdev:.4} using {param}')
-'''
+
 para_range= np.arange(0, 12)
 train_scores, validation_scores = validation_curve(model_CV, X=X, y=y, param_name="num_nodes",
                                                    param_range=para_range, scoring="accuracy")
@@ -233,45 +197,105 @@ plt.legend(loc = 'best')
 plt.savefig('figures/Validation_scores_for_ANN.png')
 plt.show()
 
+
+def create_classifier3(nodes=3):
+    classifier = keras.Sequential()
+    print("classifier initialized")
+    # Adding the input layer and the first hidden layer with dropout
+    classifier.add(Dense(units=nodes, kernel_initializer='glorot_uniform', activation='relu', input_dim=19))
+    print("input layer done")
+    classifier.add(Dropout(0.1))
+
+    # Adding the second hidden layer
+    classifier.add(Dense(units=nodes, kernel_initializer='glorot_uniform', activation='relu'))
+    print("hidden layer done")
+    classifier.add(Dropout(0.1))
+
+    # Adding the output layer
+    classifier.add(Dense(units=1, kernel_initializer='glorot_uniform', activation='sigmoid'))
+    print("about to compile")
+    # Compiling the ANN
+    classifier.compile(optimizer='adam', loss='binary_crossentropy', metrics=['acc'])
+    return classifier
+node_range = np.arange(1,13)
+model3 = KerasClassifier(build_fn=create_classifier3, verbose=1)
+train_scores, validation_scores = validation_curve(model3, X=X, y=y, param_name="nodes",
+                                                   param_range=node_range, scoring="accuracy")
+mean_train_score = np.mean(train_scores, axis = 1)
+std_train_score = np.std(train_scores, axis = 1)
+mean_test_score = np.mean(validation_scores, axis = 1)
+std_test_score = np.std(validation_scores, axis = 1)
+
+plt.plot(node_range, mean_train_score, label="Training Score", color='b')
+plt.plot(node_range, mean_test_score, label="Cross Validation Score", color='g')
+
+
+
+plt.ylabel("Accuracy")
+plt.xlabel("number of nodes in layer")
+plt.title("Validation scores for ANN")
+plt.tight_layout()
+plt.legend(loc = 'best')
+plt.savefig('figures/Validation_curve_for_ANN.png')
+plt.show()
 '''
-#classifier_history = classifier.fit(X_train, y_train, batch_size = 10, epochs = 100, validation_data=(X_test, y_test))
-#y_pred = classifier.predict(X_test)
-#y_pred = (y_pred > 0.5)
+# plotting of graphs for best params determined by RandomizedSearchcv
+
+classifier = keras.Sequential()
+print("classifier initialized")
+# Adding the input layer and the first hidden layer with dropout
+classifier.add(Dense(units=6, kernel_initializer='glorot_uniform', activation='relu', input_dim=19))
+print("input layer done")
+classifier.add(Dropout(0.1))
+
+# Adding the second hidden layer
+classifier.add(Dense(units=6, kernel_initializer='glorot_uniform', activation='relu'))
+print("hidden layer done")
+classifier.add(Dropout(0.1))
+
+# Adding the output layer
+classifier.add(Dense(units=1, kernel_initializer='glorot_uniform', activation='sigmoid'))
+print("about to compile")
+# Compiling the ANN
+classifier.compile(optimizer='adam', loss='binary_crossentropy', metrics=['acc'])
+classifier_history = classifier.fit(X_train, y_train, batch_size = 128, epochs = 60, validation_data=(X_test, y_test))
+y_pred = classifier.predict(X_test)
+y_pred = (y_pred > 0.5)
 # plotting code comes from https://towardsdatascience.com/simple-guide-to-hyperparameter-tuning-in-neural-networks-3fe03dad8594
 # Plot the loss function
-#plt.ylabel(r'Loss')
-#plt.xlabel(r'Epoch')
-#plt.title("Loss Function for ANN")
+plt.ylabel(r'Loss')
+plt.xlabel(r'Epoch')
+plt.title("Loss Function for Optimized ANN")
+
+plt.plot(classifier_history.history['loss'], 'r', label='train')
+plt.plot(classifier_history.history['val_loss'], 'b' ,label='val')
+
+plt.tight_layout()
+plt.legend(loc = 'best')
+plt.savefig('figures/Loss_function_for_optimized_ANN.png')
+plt.show()
 
 
-#fig, ax = plt.subplots(1, 1, figsize=(10,6))
-#plt.plot(np.sqrt(classifier_history.history['loss']), 'r', label='train')
-#plt.plot(np.sqrt(classifier_history.history['val_loss']), 'b' ,label='val')
-#ax.set_xlabel(r'Epoch', fontsize=20)
-#ax.set_ylabel(r'Loss', fontsize=20)
-#ax.legend()
-#plt.tight_layout()
-#plt.legend(loc = 'best')
-
-#ax.tick_params(labelsize=20)
-#plt.show()
-#plt.savefig('figures/Loss_function_for_ANN.png')
-#plt.show()
 
 # Plot the accuracy
+plt.ylabel(r'Accuracy')
+plt.xlabel(r'Epoch')
+plt.title("Accuracy Function for Optimized ANN")
 #fig, ax = plt.subplots(1, 1, figsize=(10,6))
-#plt.plot(np.sqrt(classifier_history.history['acc']), 'r', label='train')
-#plt.plot(np.sqrt(classifier_history.history['val_acc']), 'b' ,label='val')
+plt.plot(classifier_history.history['acc'], 'r', label='train')
+plt.plot(classifier_history.history['val_acc'], 'b' ,label='val')
 #plt.xlabel(r'Epoch')
 #plt.ylabel(r'Accuracy')
-#plt.legend(loc = 'best')
-#plt.title("Accuracy function for ANN")
-#ax.set_xlabel(r'Epoch', fontsize=20)
-#ax.set_ylabel(r'Accuracy', fontsize=20)
-#ax.legend()
+plt.legend(loc = 'best')
+plt.savefig('figures/Accuracy_function_for_optimized_ANN.png')
+plt.show()
 
-#plt.savefig('figures/Accuracy_function_for_ANN.png')
-#plt.show()
+
 # Making the Confusion Matrix
-from sklearn.metrics import confusion_matrix
-#cm = confusion_matrix(y_test, y_pred)
+
+cm = confusion_matrix(y_test, y_pred)
+sn.set(font_scale=1.4) # for label size
+sn.heatmap(cm, annot=True, annot_kws={"size": 16}) # font size
+
+plt.show()
+plt.savefig('figures/confusion_matrix_for_optimized_ANN')
